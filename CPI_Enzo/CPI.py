c242b5aff1ad90eab06ab470c3202e0aff7bc062
Zvorky <enzzavorski@edu.univali.br>
2022-08-28 02:56:08 -0300

    Open File

--- CPI_Enzo/CPI.py
+++ CPI_Enzo/CPI.py
@@ -6,3 +6,33 @@
 ''' UNIVALI – Arquitetura e Organização de Computadores II
     Prof. Douglas Rossi de Melo
     Avaliação 01 – Desempenho                               '''
+
+import os
+
+
+def clear():
+    if(os.getenv('OS')):
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+
+class CPI:
+    def __init__(self, filepath = ''):
+        clear()
+        if(filepath):
+            self.filepath = filepath
+        else:
+            self.filepath = filepath = input('Insira o arquivo que deseja abrir\n« ')
+        
+        try:
+            with open(filepath, 'rb') as self.file:
+                print('Arquivo carregado.\n')
+                
+        except FileNotFoundError:
+            input('Arquivo não encontrado!\n')
+            self.__init__()
+
+
+cpi = CPI()
